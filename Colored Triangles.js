d2c09aba44c2a432912f1b36284113d7bb972cc3
Leon Ebert <47148350+Veromas2k@users.noreply.github.com>
2019-05-03 11:58:56 +0200

    Add files via upload
    Start: 02.05.2019
    Ende: 03.05.2019

---
+++ Colored Triangles.js
@@ -0,0 +1,74 @@
+var stringInput = document.getElementById("stringInput");
+var string;
+var i;
+var i1;
+var newString ="";
+var para;
+var node;
+var element;
+var child;
+
+function addRed(){
+	document.getElementById("stringInput").innerHTML = document.getElementById("stringInput").innerHTML+"R";
+}
+function addGreen(){
+	document.getElementById("stringInput").innerHTML = document.getElementById("stringInput").innerHTML+"G";
+}
+function addBlue(){
+	document.getElementById("stringInput").innerHTML = document.getElementById("stringInput").innerHTML+ "B";
+}
+
+function calculate(){
+	document.getElementById("strings").innerHTML = "";
+	string = document.getElementById("stringInput").innerHTML;
+	i = string.length;
+	while( i > 1){
+		i1 = i;
+		while( i1 > 1){
+			switch(string.substr(i-i1,2)){
+				case "RR":
+					newString = newString + "R";
+					break;
+				case "GG":
+					newString = newString + "G";
+					break;
+				case "BB":
+					newString = newString + "B";
+					break;
+				case "GB":
+					newString = newString + "R";
+					break;
+				case "BR":
+					newString = newString + "G";
+					break;
+				case "RG":
+					newString = newString + "B";
+					break;
+				case "BG":
+					newString = newString + "R";
+					break;
+				case "RB":
+					newString = newString + "G";
+					break;
+				case "GR":
+					newString = newString + "B";
+					break;
+			}	
+			i1--;
+		}
+		string = newString;
+		para = document.createElement("p");
+		node = document.createTextNode(string);
+		para.appendChild(node);
+		element = document.getElementById("strings");
+		child = document.getElementById("stringBottom");
+		element.insertBefore(para,child);
+		newString ="";
+		i--;
+	}
+}
+
+function deleteS(){
+	document.getElementById("stringInput").innerHTML = "";
+	document.getElementById("strings").innerHTML = "";
+}
